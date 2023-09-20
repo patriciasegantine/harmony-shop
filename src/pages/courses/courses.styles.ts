b52fcc579ce0b90839be3f4d5e160b15dd25b006
Patricia Segantine <pnsegantine@gmail.com>
2023-09-20 21:30:27 +0100

    feat: courses updating layout

--- src/pages/courses/courses.styles.ts
+++ src/pages/courses/courses.styles.ts
@@ -13,7 +13,21 @@ export const CoursesContent = styled.div`
   display: flex;
   align-items: stretch;
   column-gap: ${theme["spacing-regular"]};
-  margin-top: ${theme["spacing-big"]};
+  margin-top: ${theme["spacing-extra-big"]};
+  
+  h3{
+    text-transform: uppercase;
+    
+    &::after {
+      content: "";
+      display: block;
+      width: 100px;
+      height: .5px;
+      margin: ${theme["spacing-small"]} 0;
+      background: ${theme["color-detached"]};
+      
+    }
+  }
 
   img {
     height: 100%;
