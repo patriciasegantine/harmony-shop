453279413cd762bde98fb11fcabd39c19cc5b415
Patricia Segantine <pnsegantine@gmail.com>
2023-09-21 23:59:32 +0100

    feat: added ThemeProvider from @mui/material

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { GlobalStyle } from "./global.styles.ts";
 import { Header } from "./components/header/header.tsx";
 import { Router } from "./router/router.tsx";
 import { Footer } from "./components/footer/footer.tsx";
+import { muiTheme } from "./muiTheme.ts";
+import { ThemeProvider } from "@mui/material";
 
 function App() {
   const MainContainer = styled.main`
@@ -13,12 +15,14 @@ function App() {
   `
   
   return (
-    <MainContainer>
-      <GlobalStyle/>
-      <Header/>
-      <Router/>
-      <Footer/>
-    </MainContainer>
+    <ThemeProvider theme={muiTheme}>
+      <MainContainer>
+        <GlobalStyle/>
+        <Header/>
+        <Router/>
+        <Footer/>
+      </MainContainer>
+    </ThemeProvider>
   )
 }
 
