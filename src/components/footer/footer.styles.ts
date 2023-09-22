0ee8be0d6e3a5665a4e69cc0524d7db533057c58
Patricia Segantine <pnsegantine@gmail.com>
2023-09-22 11:48:32 +0100

    feat: added footer

--- src/components/footer/footer.styles.ts
+++ src/components/footer/footer.styles.ts
@@ -2,15 +2,19 @@ import styled from "styled-components";
 import { theme } from "../../theme.ts";
 
 export const FooterContainer = styled.div`
-  padding: ${theme["spacing-regular"]};
+  padding-top: ${theme["spacing-regular"]};
   border-top: 1px solid ${theme["color-neutral-dark"]};
   width: 100%;
-  text-align: end;
   font-size: ${theme["font-size-s"]};
+  padding-bottom: ${theme["spacing-regular"]};
+
 `
 
 export const FooterContent = styled.footer`
   max-width: ${theme['container']};
   margin: 0 auto;
-  
+
+  img {
+    max-width: 160px;
+  }
 `
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,29 @@
 import { FooterContainer, FooterContent } from "./footer.styles.ts";
+import { useEffect } from "react";
+import logo from "../../assets/img/logo.png"
+import { Grid } from "@mui/material";
 
 export const Footer = () => {
+  
+  const thisYear = new Date().getFullYear()
+  
+  useEffect(() => {
+  
+  }, []);
+  
   return (
     <FooterContainer>
       <FooterContent>
-        <p>Developed with ❤️ by Patricia Segantine</p>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <img src={logo} alt=""/>
+          </Grid>
+          
+          <Grid item xs={6} textAlign={"end"}>
+            <div>{`© ${thisYear} - Developed with ❤️ by Patricia Segantine`}</div>
+          </Grid>
+        
+        </Grid>
       </FooterContent>
     </FooterContainer>
   );
